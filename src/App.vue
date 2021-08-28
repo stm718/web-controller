03ce0ea501d2b225dfffd1345da5af3c43cbc4b4
Ubuntu <ubuntu@ip-172-31-26-131.ap-northeast-1.compute.internal>
2021-08-28 16:06:32 +0000

    Add a subscription

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <div>
+        <h1>Web Controller</h1>
+      </div>
+      <div>
+        Temperture: {{temperture}}
+      </div>
+      <!--<div>-->
+      <!--  Acc: {{acc}}-->
+      <!--</div>-->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import awsconfig from './aws-exports'
+Amplify.configure(awsconfig)
+
+import Amplify, { PubSub } from 'aws-amplify';
+import { AWSIoTProvider } from '@aws-amplify/pubsub/lib/Providers';
+
+Amplify.addPluggable(new AWSIoTProvider({
+  aws_pubsub_region: 'ap-northeast-1',
+  aws_pubsub_endpoint: 'wss://a361rf3knfpqtt-ats.iot.ap-northeast-1.amazonaws.com/mqtt',
+}));
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: () => {
+    return {
+      temperture: "value",
+      // acc: "value"
+    }
+  },
+  mounted: async function () {
+    PubSub.subscribe('data/sensor').subscribe({
+      next: data => {
+        this.temperture = data.value.data
+        console.log('Message received', data)
+      },
+      error: error => console.error(error),
+      close: () => console.log('Done'),
+    });
+  },
+  methods: {
+    publish() {
+      PubSub.publish('cmd/device', "cmd");
+    }
   }
 }
 </script>
