be3b31bfd1609bf0afeb0c50b6baab075bac3e21
stm718 <stm7185@gmail.com>
2021-09-23 13:38:54 +0000

    [update] Add an input form for angles

---
+++ src/components/Inputform.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+      <h2>Angle</h2>
+      <input
+        v-model.number="angle"
+        type="number"
+        :min="0"
+        :max="180"
+        >
+    </div>
+</template>
+
+<script>
+  export default {
+    data: () => {
+      return {
+        angle: 0,
+        message: null,
+        changeEnable: true
+      }
+    },
+    watch: {
+      angle: function(newNum) {
+        if ( newNum < 0 || newNum > 180  ) {
+          alert('Illegal angle')
+          this.angle = 0
+          this.changeEnable=false
+        } else {
+          this.changeEnable=true
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  h2 {
+    color: black
+  }
+</style>
