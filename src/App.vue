049ba7fb066d4ac958dfe7d025147065f2af9479
stm718 <stm7185@gmail.com>
2021-10-03 00:59:17 +0000

    [update] Export the Amplify confituration to aws-exports

--- src/App.vue
+++ src/App.vue
@@ -12,46 +12,12 @@
 <script>
 import Amplify from 'aws-amplify';
 import Header from '@/components/Header'
+import awsmobile from './aws-exports'
 
-Amplify.configure({
-  aws_project_region: "ap-northeast-1",
-  aws_cognito_identity_pool_id: "ap-northeast-1:d3bbbae2-04a8-4757-9b34-ea9b7cd857f9",
-  aws_cognito_region: "ap-northeast-1",
-  aws_user_pools_id: "ap-northeast-1_Kl2FaZTQk",
-  aws_user_pools_web_client_id: "gihec5klm1ofl5nclmmb3m1hu",
-  oauth: {},
-  aws_cognito_login_mechanisms: [
-      "EMAIL"
-  ],
-  aws_cognito_signup_attributes: [
-      "EMAIL"
-  ],
-  aws_cognito_mfa_configuration: "OFF",
-  aws_cognito_mfa_types: [
-      "SMS"
-  ],
-  aws_cognito_password_protection_settings: {
-      "passwordPolicyMinLength": 8,
-      "passwordPolicyCharacters": []
-  },
-})
-
-if( process.env.VUE_APP_ENVIRONMENT === 'staging') {
-  Amplify.configure({
-    aws_appsync_region: "ap-northeast-1",
-    aws_appsync_graphqlEndpoint: process.env.VUE_APP_APPSYNC_GRAPHQLENDPOINT,
-    aws_appsync_authenticationType: "API_KEY",
-    aws_appsync_apiKey: process.env.VUE_APP_APPSYNC_APIKEY,
-  });
-} else if (process.env.VUE_APP_ENVIRONMENT === 'development'){
+if (process.env.VUE_APP_ENVIRONMENT === 'development'){
   console.warn("Local mode")
-  Amplify.configure({
-    aws_appsync_region: "ap-northeast-1",
-    aws_appsync_graphqlEndpoint: process.env.VUE_APP_APPSYNC_GRAPHQLENDPOINT,
-    aws_appsync_authenticationType: "API_KEY",
-    aws_appsync_apiKey: process.env.VUE_APP_APPSYNC_APIKEY,
-  });
 }
+Amplify.configure(awsmobile);
 
 export default {
   name: 'App',
